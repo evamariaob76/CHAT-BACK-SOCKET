35ba0d2ec1d591f5f0d70926b7754e43e41c6e5d
Eva Ortiz <evamariaob76.com>
2022-06-16 19:47:02 +0200

    cambio variables

--- models/server.js
+++ models/server.js
@@ -16,7 +16,7 @@ class Server {
         this.app  = express();
         this.app.use(timeout('5s'))
 
-        this.port = process.env.PORT || 8080;
+        this.port = process.env.PORT || 80;
 
         
         //Conectar a BD
